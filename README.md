2742009b8933d9d32ef01f49bcd474c42765d211
jilberto@umich.edu <jilberto@umich.edu>
2023-09-10 14:52:59 -0400

    renaming folders and updating readme

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Before starting, install conda or miniconda (either is fine, miniconda is a ligh
 
 Once conda is installed, create an environment by running the following,
 ```
-conda create -n cardiac-mechanics python=3.10 
+conda create -n cardiac-mechanics python=3.10
 ```
-Here, `cardiac-mechanics` is the name of the environment. Now activate the environment and install fenics-x and all the dependencies required to run the tutorial (basically, fenics-x and jupyter). This step might take a while, depending on your system. 
+Here, `cardiac-mechanics` is the name of the environment. Now activate the environment and install fenics-x and all the dependencies required to run the tutorial (basically, fenics-x and jupyter). This step might take a while, depending on your system.
 ```
 conda activate cardiac-mechanics
 conda install -c conda-forge fenics-dolfinx mpich jupyter
@@ -18,15 +18,25 @@ Clone the repository (this will make a folder from wherever folder you are calli
 ```
 git clone https://github.com/javijv4/cardiac-mechanics-tutorial
 ```
+To run the second tutorial, an specialized package [ambit](https://github.com/marchirschvogel/ambit) is needed. This can be installed using (make sure the cardiac-mechanics environment is activated),
+```
+python -m pip install ambit-fe==1.0.7
+```
 
-## Running the tutorial
-Every time you want to run the tutorial after the first time, you need to activate the environment and call jupyter.
+## Tutorials
+Before running any tutorial make sure to activate the environment,
 ```
 conda activate cardiac-mechanics
-jupyter-notebook tutorial.ipynb
 ```
+Each tutorial has a `python` file (`.py`) and a `jupyter-notebook` version (`.ipynb`). The code is the same, but the `jupyter-notebook` has more information about the steps.
+
+### Tutorial 1 - Simple Left Ventricle
+This tutorial consists in an idealized left ventricle geometry. For the tutorial purposes a very coarse discretization is used. Chamber pressure and cardiac muscle activation curves are given to recreate the cardiac cycle. The provided `python` and `jupyter-notebook` files detail the setup of the problem, from reading the input files, the hyperelastic formulation, boundary conditions and solver setup.
+
+### Tutorial 2 - Coupled Left Ventricle
+In this tutorial the same setup as Tutorial 1 is used but instead of prescribing a chamber pressure, the solid 3D model is coupled with a 0D representation of the cardiovascular system. This is a more complex setup, so instead of directly using fenics-x to set up the problem, we use the package [ambit](https://github.com/marchirschvogel/ambit) that is also based on fenics-x. The provided `python` and `jupyter-notebook` files detail the setup of the file and options required to setup the 3D-0D problem.
 
 ## Resources
 For more examples of how to use fenics-x, we recommend looking at this comprehensive [tutorial](https://jsdokken.com/dolfinx-tutorial/).
 
-The cardiac mechanics implementation shown here is loosely based on the following repositories: [fenicsx-pulse](https://github.com/finsberg/fenicsx-pulse) and [ambit](https://github.com/marchirschvogel/ambit).
+The cardiac mechanics implementation in Tutorial 1 is loosely based on the following repositories: [fenicsx-pulse](https://github.com/finsberg/fenicsx-pulse) and [ambit](https://github.com/marchirschvogel/ambit).
---
+++ tutorial1_simple_lv/cardiac_cycle.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Cardiac Mechanics Tutorial w/ FeniCS-x
+Cardiac Biomechanics Lab @ UofM
+Prof. David Nordsletten
+
+@author: Javiera Jilberto Vallejos (jilberto@umich.edu)
+"""
+import numpy as np
+import ufl
+from mpi4py import MPI
+from petsc4py import PETSc
+from dolfinx import io, fem, nls
+import os
+import time
+
+data_path = 'data/'
+
+# Read mesh
+with io.XDMFFile(MPI.COMM_WORLD, data_path + 'mesh.xdmf', "r") as xdmf:
+    domain = xdmf.read_mesh(name='Grid')
+
+# Read meshtags (boundary information)
+domain.topology.create_connectivity(domain.topology.dim-1, domain.topology.dim)
+with io.XDMFFile(MPI.COMM_WORLD, data_path + 'mt.xdmf', "r") as xdmf:
+    meshtags = xdmf.read_meshtags(domain, name="Grid")
+
+# Define spaces
+order_u = 1      # polynomial degree for displacement
+order_p = 1      # polynomial degree for pressure
+Ve = ufl.VectorElement("Lagrange", domain.ufl_cell(), order_u)
+Pe = ufl.FiniteElement("Lagrange", domain.ufl_cell(), order_p)
+state_space = fem.FunctionSpace(domain, Ve * Pe)
+
+# Define functions and test functions
+state = fem.Function(state_space)
+test_state = ufl.TestFunction(state_space)
+
+u, p = ufl.split(state)
+v, q = ufl.split(test_state)
+
+# Fibers
+V_f = fem.VectorFunctionSpace(domain, ('CG', 1))       # space for fibers - continuous linear space
+T_f = fem.TensorFunctionSpace(domain, ('CG', 1))       # space for outer product - continuous linear space
+
+f0_array = np.loadtxt(data_path + 'fiber.txt')
+s0_array = np.loadtxt(data_path + 'sheet.txt')
+n0_array = np.loadtxt(data_path + 'sheetnormal.txt')
+
+# pre compute outer product
+outer_ff = np.zeros([*f0_array.shape, 3])
+for i in range(len(f0_array)):
+    outer_ff[i] = np.outer(f0_array[i], f0_array[i])
+ff = fem.Function(T_f)
+ff.vector.array = outer_ff.flatten()
+
+f0 = fem.Function(V_f)    # In this tutorial fibers are defined in a linear topology
+s0 = fem.Function(V_f)
+n0 = fem.Function(V_f)
+
+f0.vector.array = f0_array.flatten()
+s0.vector.array = s0_array.flatten()
+n0.vector.array = n0_array.flatten()
+
+# Kinematics
+dim = len(u)
+I = ufl.variable(ufl.Identity(dim))
+F = ufl.variable(I + ufl.grad(u))         # Deformation gradient
+C = ufl.variable(F.T*F)                   # Right Green-Cauchy tensor
+
+# Invariants
+J = ufl.variable(ufl.det(F))
+Ic = ufl.variable(ufl.tr(C))
+Ic_bar = ufl.variable(J**(-2./3.) * Ic)
+I4f = ufl.inner(C, ff)
+I4s = ufl.inner(C*s0, s0)
+
+# Define material law
+a = fem.Constant(domain, PETSc.ScalarType(0.4))
+b = fem.Constant(domain, PETSc.ScalarType(3.2))
+a_f = fem.Constant(domain, PETSc.ScalarType(1.0))
+b_f = fem.Constant(domain, PETSc.ScalarType(15.))
+Psi_dev = a/(2.*b)*(ufl.exp(b*(Ic_bar-3.)) - 1.) \
+    + a_f/(2.*b_f)*(ufl.exp(b_f*(I4f-1.)**2.) - 1.)
+
+k = fem.Constant(domain, PETSc.ScalarType(1e2))
+Psi_vol = p * (J - 1) - p**2 / (2*k)
+
+# Get PK1 stress
+P_dev = ufl.diff(Psi_dev,F)
+P_vol = ufl.diff(Psi_vol,F)
+
+# Define active stress
+f = F*f0
+phi = ufl.tanh(3.75*(ufl.sqrt(I4f)-0.8))    # length dependent activation
+lv_activation = fem.Constant(domain, PETSc.ScalarType(0.))
+P_act = lv_activation * phi * ufl.outer(f,f0)
+
+# Boundary conditions
+# Fixed base
+V, _ = state_space.sub(0).collapse()
+dofs = fem.locate_dofs_topological((state_space.sub(0), V), 2, meshtags.find(2))
+u_fixed = fem.Function(V)
+u_fixed.x.set(0.0)
+bcs = [fem.dirichletbc(u_fixed, dofs, state_space.sub(0))]
+
+# Endocardial pressure
+normal_0 = ufl.FacetNormal(domain)
+lv_pressure = fem.Constant(domain, PETSc.ScalarType(0.))
+T = -lv_pressure * J * ufl.inv(F).T * normal_0
+
+# Define integration domains
+ds = ufl.Measure('ds', domain=domain, subdomain_data=meshtags)
+dx = ufl.Measure("dx", domain=domain, metadata={'quadrature_degree': 4})
+
+# Virtual work
+deltaW_uu = ufl.inner(ufl.grad(v), P_dev)*dx + ufl.inner(ufl.grad(v), P_act)*dx - ufl.inner(v, T)*ds(3)
+deltaW_up = ufl.inner(ufl.grad(v), P_vol)*dx
+deltaW_pu = q * (J - 1.) * dx
+deltaW_pp = - 1/k * p * q * dx
+deltaW = deltaW_uu + deltaW_up + deltaW_pu + deltaW_pp
+
+
+# Setting up the problem and solver
+problem = fem.petsc.NonlinearProblem(deltaW, state, bcs)
+solver = nls.petsc.NewtonSolver(domain.comm, problem)
+solver.atol = 1e-8
+solver.rtol = 1e-8
+solver.error_on_nonconvergence = False
+
+# Read data for activation and pressure
+activation = np.loadtxt(data_path + 'activation.txt')
+pressure = np.loadtxt(data_path + 'pressure.txt')
+
+from scipy.interpolate import interp1d
+func_act = interp1d(activation[:,0], activation[:,1])
+func_pres = interp1d(pressure[:,0], pressure[:,1])
+
+
+# First step: inflate LV to initial pressure
+# Initializing files to save results (clean first)
+if os.path.isfile('inflation.xdmf'):
+    os.remove('inflation.xdmf')
+    os.remove('inflation.h5')
+
+disp_file = io.XDMFFile(MPI.COMM_WORLD, 'inflation.xdmf', 'w')
+disp_file.write_mesh(domain)
+
+# Incremmentally load the lv using 10 timesteps
+nsteps = 21
+pres_values = np.linspace(0, func_pres(0), nsteps+1)[1:]
+for i in range(len(pres_values)):
+    lv_pressure.value = pres_values[i]
+    num_its, converged = solver.solve(state)
+    if not converged:
+        disp_file.close()
+        assert converged
+    u, p = state.split()
+    disp_file.write_function(u, i)
+    print(f"Time step {i}, Number of iterations {num_its}, pressure {lv_pressure.value}")
+disp_file.close()
+
+inf_state = fem.Function(state_space)
+inf_state.vector.array = np.copy(state.vector.array)
+
+# Second step: Cardiac cycle
+state.vector.array = np.copy(inf_state.vector.array)
+
+if os.path.isfile('cycle.xdmf'):
+    os.remove('cycle.xdmf')
+    os.remove('cycle.h5')
+disp_file = io.XDMFFile(MPI.COMM_WORLD, 'cycle.xdmf', 'w')
+disp_file.write_mesh(domain)
+
+start = time.time()
+nsteps = 501
+times = np.linspace(0, 1, nsteps)[1:]    # We already solved the first step
+for i, t in enumerate(times):
+    # Set pressure/activation values
+    lv_pressure.value = func_pres(t)
+    lv_activation.value = func_act(t)
+
+    # Solve
+    num_its, converged = solver.solve(state)
+
+    if not converged:
+        disp_file.close()
+        assert converged
+
+    # Save output
+    u, p = state.split()
+    disp_file.write_function(u, i)
+
+    print(f"Time step {i}, Number of iterations {num_its}, pressure {lv_pressure.value}, activation {lv_activation.value}")
+disp_file.close()
+
+end = time.time()
+
+print(f"Simulation time {end-start} s")
+
+f0.vector.destroy()
+s0.vector.destroy()
+n0.vector.destroy()
+ff.vector.destroy()
+u.vector.destroy()
